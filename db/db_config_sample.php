a0215fcf3c7a77664ff027c7cf422a81f00912db
nikolina <nikolinap85@gmail.com>
2018-03-20 12:04:54 +0000

    DB config explanation changed

--- db/db_config_sample.php
+++ db/db_config_sample.php
@@ -1,8 +1,9 @@
 <?php
 /**
     Use this file to configure your database connection. Do the following:
-    1. Uncomment the code below and enter the name of your DB host and credentials of your DB account (username and password).
-    2. Save changes and rename this file to db_config.php.
+    1. Copy this file and rename it to db_config.php; 
+    2. Open db_config.php, uncomment the code below and enter the name of your DB host and credentials of your DB account (username and password);
+    3. Save changes to db_config.php.
 */
 //$GLOBALS['serverName'] = "name_of_the_host";
 //$GLOBALS['dbUser'] = "DB_username";
