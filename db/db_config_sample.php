ec9ce8592f78a331f7edf93a3d2ba4bf42ab5780
nikolina <nikolinap85@gmail.com>
2018-03-20 11:28:41 +0000

    Adding sample DB configuration file

---
+++ db/db_config_sample.php
@@ -0,0 +1,10 @@
+<?php
+/**
+    Use this file to configure your database connection. Do the following:
+    1. Uncomment the code below and enter the name of your DB host and credentials of your DB account (username and password).
+    2. Save changes and rename this file to db_config.php.
+*/
+//$GLOBALS['serverName'] = "name_of_the_host";
+//$GLOBALS['dbUser'] = "DB_username";
+//$GLOBALS['dbPass'] = "DB_user's_password";
+$GLOBALS['indexPage'] = "index.php";
